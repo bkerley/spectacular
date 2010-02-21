2a0d1445977b1eb9915f473cdcc528f5255ff9eb
Bryce Kerley <bkerley@brycekerley.net>
2010-02-20 20:48:02 -0500

    more exception info

--- lib/spectacular/app.rb
+++ lib/spectacular/app.rb
@@ -6,26 +6,37 @@ module Spectacular
     def call(environment)
       method = environment['REQUEST_METHOD']
       path = environment['PATH_INFO']
-      @dispatcher.dispatch
-    rescue => e
-      error_backtrace e
+      @dispatcher.dispatch method, path, environment
+    rescue => exception
+      error_backtrace exception, environment
     end
 
     private
-    def error_backtrace(exception)
+    def error_backtrace(exception, environment)
       exception_info = []
       exception_info << <<-EOS
         <html><head><title>Spectacular Exception</title></head><body>
         <h1>Spectacular Exception</h1>
-        <h2>#{CGI.escapeHTML exception.to_s}</h2>
+        <h2>#{escape exception}</h2>
+        <h3>Traceback</h3>
         <ol>
       EOS
-      exception_info += exception.backtrace.map{ |e| "<li>#{CGI.escapeHTML e.to_s}</li>"}
+      exception_info += exception.backtrace.map{ |e| "<li>#{escape e}</li>"}
       exception_info << <<-EOS
-        </ol></body></html>
+        </ol>
+        <h3>Environment</h3>
+        <ul>
+      EOS
+      exception_info += environment.to_a.map{ |e| "<li>#{escape e[0]}: #{escape e[1]}</li>"}
+      exception_info << <<-EOS
+        </body></html>
       EOS
 
       [500, { 'Content-type' => 'text/html'}, exception_info]
+    end
+
+    def escape(string)
+      CGI.escapeHTML string.to_s
     end
   end
 end
--- test/exception_test.rb
+++ test/exception_test.rb
@@ -4,11 +4,16 @@ class ExceptionTest < Test::Unit::TestCase
   context 'A failed request' do
     setup do
       app.instance_variable_get('@dispatcher').expects(:dispatch).raises(StandardError, 'exception test error')
-      get '/'
+      get '/exception_test'
     end
 
     should 'return the message of the exception' do
       assert_match 'exception test error', last_response.body
+    end
+
+    should 'return environment information' do
+      assert_match 'REQUEST_METHOD: GET', last_response.body
+      assert_match 'PATH_INFO: /exception_test', last_response.body
     end
   end
 end
