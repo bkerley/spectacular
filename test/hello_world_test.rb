981763fe1ceb727143f2d686da29698cb0397a3e
Bryce Kerley <bkerley@brycekerley.net>
2010-02-20 21:17:29 -0500

    pass dispatch test 8)

--- test/hello_world_test.rb
+++ test/hello_world_test.rb
@@ -2,6 +2,6 @@ require(File.join(File.dirname(__FILE__), 'test_helper.rb'))
 class HelloWorldTest < Test::Unit::TestCase
   def test_index_is_hello_world
     get '/'
-    assert_equal 'hello world', last_response.body
+    assert_match 'hello world', last_response.body
   end
 end
