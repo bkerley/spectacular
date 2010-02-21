6e4b59fc51f328043fab707a459f04083240d6fd
Bryce Kerley <bkerley@brycekerley.net>
2010-02-20 20:48:15 -0500

    shorter hello world test

--- test/hello_world_test.rb
+++ test/hello_world_test.rb
@@ -1,7 +1,5 @@
 require(File.join(File.dirname(__FILE__), 'test_helper.rb'))
 class HelloWorldTest < Test::Unit::TestCase
-  include Rack::Test::Methods
-
   def test_index_is_hello_world
     get '/'
     assert_equal 'hello world', last_response.body
