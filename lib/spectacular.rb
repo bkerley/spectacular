9cfb9609473d056647f50b6b0838f5b661c0db09
Bryce Kerley <bkerley@brycekerley.net>
2010-02-20 21:09:26 -0500

    almost dispatching

--- lib/spectacular.rb
+++ lib/spectacular.rb
@@ -2,3 +2,5 @@ require 'cgi'
 %w{ dispatcher controller app }.each do |f|
   require File.join(File.dirname(__FILE__), "spectacular/#{f}.rb")
 end
+
+APP_DIR = File.join(File.dirname(__FILE__), '../app')
--- lib/spectacular/dispatcher.rb
+++ lib/spectacular/dispatcher.rb
@@ -1,5 +1,50 @@
 module Spectacular
   class Dispatcher
+    def initialize
+      load_controllers
+      load_routes
+    end
 
+    def dispatch(method, path, environment)
+      matched_path, controller, method = get_route path
+
+      controller.response_for method
+    end
+
+    private
+    def load_controllers
+      controller_files = Dir.glob(File.join(APP_DIR, 'controllers', '*_controller.rb'))
+      controller_files.each { |f| require f }
+    end
+
+    def load_routes
+      route_filename = File.join(APP_DIR, 'routes.rb')
+      route_data = File.read(route_filename)
+      route_pen = RoutePen.new
+      eval route_data, route_pen.get_binding, route_filename
+      @routes = route_pen.to_a
+    end
+
+    def get_route(path)
+      @routes.detect{ |r| path === r[0] }
+    end
+
+    class RoutePen
+      def initialize
+        @routes = []
+      end
+
+      def get_binding
+        binding
+      end
+
+      def route(path, controller, method)
+        @routes << [path, controller, method]
+      end
+
+      def to_a
+        @routes
+      end
+    end
   end
 end
